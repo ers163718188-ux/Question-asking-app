067ea427890b72aa66da5a6a8b68f710cc8190f0
The bigginer <ers163718188@gmail.com>
2026-01-14 13:49:55 +0530

    Add files via upload

--- the best tk I have ever made.py
+++ the best tk I have ever made.py
@@ -2,22 +2,24 @@ import tkinter as tk
 from tkinter import simpledialog, messagebox
 
 root = tk.Tk()
-root.withdraw()  
+root.withdraw()
 
-print (messagebox.showinfo("Hi", "How are you?"))# you can remove print function if you want it will doesn't affect the program 
+messagebox.showinfo("Hi", "How are you?")
 
-
+# START
 start = ""
 while start.lower().strip() != "start":
     start = simpledialog.askstring("Input", "Write start to start the test")
-    
     if start.lower().strip() != "start":
         messagebox.showerror("Error", "Error! Please try again")
 
-
+# Question 1 (UNCHANGED)
 answer = ""
 while answer != "gaya bh1":
-    answer = simpledialog.askstring("INPUT", "3. Which is the nearest blackhole to earth?")
+    answer = simpledialog.askstring(
+        "INPUT",
+        "1. Which is the nearest black hole to Earth?"
+    )
     if answer is None:
         break
     answer = answer.lower().strip()
@@ -26,19 +28,15 @@ while answer != "gaya bh1":
     else:
         messagebox.showinfo("", "CORRECT")
 
-
+# Question 2
 answerA = ""
 question = (
-"2.Rational numbers are in the form of:\n"
-"A. p*q\n"
-"B. p+q\n"
-"C. p/q\n"
-"D. p-q\n"
-"CHOOSE THE CORRECT OPTION"
+"2. Which type of numbers include fractions?\n"
+"A. Natural\nB. Whole\nC. Rational\nD. Integers"
 )
 
 while answerA.lower().strip() != "c":
-    answerA = simpledialog.askstring("Question no.2", question)
+    answerA = simpledialog.askstring("Question 2", question)
     if answerA is None:
         break
     if answerA.lower().strip() != "c":
@@ -46,37 +44,31 @@ while answerA.lower().strip() != "c":
     else:
         messagebox.showinfo("", "CORRECT")
 
-
+# Question 3
 ans = ""
 questionB = (
-"3. The centre of a black hole\n"
-"is known as (choose the correct option)\n"
-"a} black horizon\n"
-"b} event horizon\n"
-"c} middle horizon\n"
-"d} black point\n"
+"3. What is the boundary of a black hole called?\n"
+"a) Gravity wall\nb) Event horizon\nc) Dark ring\nd) Core"
 )
 
 while ans.lower().strip() != "b":
-    ans = simpledialog.askstring("Question no.3", questionB)
+    ans = simpledialog.askstring("Question 3", questionB)
     if ans is None:
         break
     if ans.lower().strip() != "b":
-        messagebox.showwarning("Error", "Please try again!!!")
+        messagebox.showerror("Error", "Please try again!!!")
     else:
         messagebox.showinfo("", "CORRECT")
 
-
-
-from tkinter import simpledialog, messagebox
-
 # Question 4
 while True:
-    a = simpledialog.askstring("Question 4",
-        "4. Sharpest natural object\n"
-        "a) Obsidian\nb) Iron\nc) Diamond\nd) Magnet")
+    a = simpledialog.askstring(
+        "Question 4",
+        "4. Which planet has the maximum number of moons?\n"
+        "a) Earth\nb) Jupiter\nc) Saturn\nd) Mars"
+    )
     if a is None: break
-    elif a.lower().strip() == "a":
+    elif a.lower().strip() == "c":
         messagebox.showinfo("Correct", "Correct!")
         break
     else:
@@ -84,11 +76,13 @@ while True:
 
 # Question 5
 while True:
-    a = simpledialog.askstring("Question 5",
-        "5. Largest planet in the solar system\n"
-        "a) Earth\nb) Jupiter\nc) Saturn\nd) Mars")
+    a = simpledialog.askstring(
+        "Question 5",
+        "5. Which vitamin is produced by sunlight?\n"
+        "a) A\nb) B\nc) C\nd) D"
+    )
     if a is None: break
-    elif a.lower().strip() == "b":
+    elif a.lower().strip() == "d":
         messagebox.showinfo("Correct", "Correct!")
         break
     else:
@@ -96,11 +90,13 @@ while True:
 
 # Question 6
 while True:
-    a = simpledialog.askstring("Question 6",
-        "6. Fastest land animal\n"
-        "a) Cheetah\nb) Lion\nc) Horse\nd) Tiger")
+    a = simpledialog.askstring(
+        "Question 6",
+        "6. SI unit of force?\n"
+        "a) Joule\nb) Pascal\nc) Newton\nd) Watt"
+    )
     if a is None: break
-    elif a.lower().strip() == "a":
+    elif a.lower().strip() == "c":
         messagebox.showinfo("Correct", "Correct!")
         break
     else:
@@ -108,9 +104,11 @@ while True:
 
 # Question 7
 while True:
-    a = simpledialog.askstring("Question 7",
-        "7. Hardest natural substance\n"
-        "a) Gold\nb) Iron\nc) Diamond\nd) Quartz")
+    a = simpledialog.askstring(
+        "Question 7",
+        "7. Gas used in photosynthesis?\n"
+        "a) Oxygen\nb) Nitrogen\nc) Carbon dioxide\nd) Hydrogen"
+    )
     if a is None: break
     elif a.lower().strip() == "c":
         messagebox.showinfo("Correct", "Correct!")
@@ -120,11 +118,13 @@ while True:
 
 # Question 8
 while True:
-    a = simpledialog.askstring("Question 8",
-        "8. Sharpest natural object\n"
-        "a) Obsidian\nb) Iron\nc) Diamond\nd) Magnet")
+    a = simpledialog.askstring(
+        "Question 8",
+        "8. Capital of Australia?\n"
+        "a) Sydney\nb) Melbourne\nc) Canberra\nd) Perth"
+    )
     if a is None: break
-    elif a.lower().strip() == "a":
+    elif a.lower().strip() == "c":
         messagebox.showinfo("Correct", "Correct!")
         break
     else:
@@ -132,11 +132,13 @@ while True:
 
 # Question 9
 while True:
-    a = simpledialog.askstring("Question 9",
-        "9. Largest ocean in the world\n"
-        "a) Atlantic\nb) Indian\nc) Arctic\nd) Pacific")
+    a = simpledialog.askstring(
+        "Question 9",
+        "9. Metal liquid at room temperature?\n"
+        "a) Iron\nb) Mercury\nc) Aluminium\nd) Copper"
+    )
     if a is None: break
-    elif a.lower().strip() == "d":
+    elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
     else:
@@ -144,11 +146,13 @@ while True:
 
 # Question 10
 while True:
-    a = simpledialog.askstring("Question 10",
-        "10. Fastest bird\n"
-        "a) Falcon\nb) Eagle\nc) Sparrow\nd) Owl")
+    a = simpledialog.askstring(
+        "Question 10",
+        "10. Largest bone in human body?\n"
+        "a) Skull\nb) Femur\nc) Rib\nd) Spine"
+    )
     if a is None: break
-    elif a.lower().strip() == "a":
+    elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
     else:
@@ -156,10 +160,13 @@ while True:
 
 # Question 11
 while True:
-    a = simpledialog.askstring("Question 11",
-        "11. Largest mammal\n"
-        "a) Elephant\nb) Blue Whale\nc) Giraffe\nd) Hippo")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 11",
+        "11. Which is the largest mammal?\n"
+        "a) Elephant\nb) Blue Whale\nc) Giraffe\nd) Hippo"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -168,10 +175,13 @@ while True:
 
 # Question 12
 while True:
-    a = simpledialog.askstring("Question 12",
-        "12. Hottest planet\n"
-        "a) Mercury\nb) Venus\nc) Mars\nd) Jupiter")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 12",
+        "12. Which planet is the hottest?\n"
+        "a) Mercury\nb) Venus\nc) Mars\nd) Jupiter"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -180,10 +190,13 @@ while True:
 
 # Question 13
 while True:
-    a = simpledialog.askstring("Question 13",
-        "13. Tallest mountain\n"
-        "a) K2\nb) Kangchenjunga\nc) Everest\nd) Lhotse")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 13",
+        "13. Which is the tallest mountain in the world?\n"
+        "a) K2\nb) Kangchenjunga\nc) Everest\nd) Lhotse"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "c":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -192,10 +205,13 @@ while True:
 
 # Question 14
 while True:
-    a = simpledialog.askstring("Question 14",
-        "14. Smallest country\n"
-        "a) Monaco\nb) Vatican City\nc) Nauru\nd) San Marino")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 14",
+        "14. Which is the smallest country?\n"
+        "a) Monaco\nb) Vatican City\nc) Nauru\nd) Malta"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -204,10 +220,13 @@ while True:
 
 # Question 15
 while True:
-    a = simpledialog.askstring("Question 15",
-        "15. Lightest element\n"
-        "a) Hydrogen\nb) Helium\nc) Lithium\nd) Oxygen")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 15",
+        "15. Which is the lightest element?\n"
+        "a) Hydrogen\nb) Helium\nc) Lithium\nd) Oxygen"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "a":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -216,10 +235,13 @@ while True:
 
 # Question 16
 while True:
-    a = simpledialog.askstring("Question 16",
-        "16. Largest desert\n"
-        "a) Sahara\nb) Gobi\nc) Arabian\nd) Antarctic")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 16",
+        "16. Which is the largest desert in the world?\n"
+        "a) Sahara\nb) Gobi\nc) Arabian\nd) Antarctica"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "d":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -228,10 +250,13 @@ while True:
 
 # Question 17
 while True:
-    a = simpledialog.askstring("Question 17",
-        "17. Fastest marine animal\n"
-        "a) Swordfish\nb) Sailfish\nc) Shark\nd) Tuna")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 17",
+        "17. Which is the fastest sea animal?\n"
+        "a) Shark\nb) Sailfish\nc) Dolphin\nd) Whale"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -240,10 +265,13 @@ while True:
 
 # Question 18
 while True:
-    a = simpledialog.askstring("Question 18",
-        "18. Most abundant gas\n"
-        "a) Oxygen\nb) CO2\nc) Nitrogen\nd) Hydrogen")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 18",
+        "18. Which gas is most abundant in Earth's atmosphere?\n"
+        "a) Oxygen\nb) Carbon dioxide\nc) Nitrogen\nd) Hydrogen"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "c":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -252,10 +280,13 @@ while True:
 
 # Question 19
 while True:
-    a = simpledialog.askstring("Question 19",
-        "19. National bird of India\n"
-        "a) Peacock\nb) Eagle\nc) Sparrow\nd) Parrot")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 19",
+        "19. What is the national bird of India?\n"
+        "a) Peacock\nb) Eagle\nc) Sparrow\nd) Parrot"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "a":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -264,10 +295,13 @@ while True:
 
 # Question 20
 while True:
-    a = simpledialog.askstring("Question 20",
-        "20. Coldest planet\n"
-        "a) Neptune\nb) Uranus\nc) Saturn\nd) Jupiter")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 20",
+        "20. Which is the coldest planet?\n"
+        "a) Neptune\nb) Uranus\nc) Saturn\nd) Jupiter"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -276,10 +310,13 @@ while True:
 
 # Question 21
 while True:
-    a = simpledialog.askstring("Question 21",
-        "21. Smallest planet\n"
-        "a) Mercury\nb) Mars\nc) Venus\nd) Earth")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 21",
+        "21. Which is the smallest planet?\n"
+        "a) Mercury\nb) Mars\nc) Venus\nd) Earth"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "a":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -288,10 +325,13 @@ while True:
 
 # Question 22
 while True:
-    a = simpledialog.askstring("Question 22",
-        "22. Fastest fish\n"
-        "a) Tuna\nb) Marlin\nc) Sailfish\nd) Swordfish")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 22",
+        "22. Which fish is the fastest?\n"
+        "a) Tuna\nb) Marlin\nc) Sailfish\nd) Swordfish"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "c":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -300,10 +340,13 @@ while True:
 
 # Question 23
 while True:
-    a = simpledialog.askstring("Question 23",
-        "23. Longest river\n"
-        "a) Nile\nb) Amazon\nc) Yangtze\nd) Mississippi")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 23",
+        "23. Which is the longest river?\n"
+        "a) Nile\nb) Amazon\nc) Yangtze\nd) Mississippi"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -312,10 +355,13 @@ while True:
 
 # Question 24
 while True:
-    a = simpledialog.askstring("Question 24",
-        "24. Largest moon\n"
-        "a) Titan\nb) Ganymede\nc) Callisto\nd) Europa")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 24",
+        "24. Which is the largest moon?\n"
+        "a) Titan\nb) Ganymede\nc) Europa\nd) Callisto"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -324,10 +370,13 @@ while True:
 
 # Question 25
 while True:
-    a = simpledialog.askstring("Question 25",
-        "25. Hottest planet\n"
-        "a) Mercury\nb) Venus\nc) Mars\nd) Jupiter")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 25",
+        "25. Which planet has the most rings?\n"
+        "a) Jupiter\nb) Saturn\nc) Uranus\nd) Neptune"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -336,10 +385,13 @@ while True:
 
 # Question 26
 while True:
-    a = simpledialog.askstring("Question 26",
-        "26. Largest bird\n"
-        "a) Ostrich\nb) Eagle\nc) Albatross\nd) Flamingo")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 26",
+        "26. Which is the largest bird?\n"
+        "a) Ostrich\nb) Eagle\nc) Albatross\nd) Flamingo"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "a":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -348,10 +400,13 @@ while True:
 
 # Question 27
 while True:
-    a = simpledialog.askstring("Question 27",
-        "27. Strongest muscle\n"
-        "a) Heart\nb) Jaw\nc) Quadriceps\nd) Gluteus")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 27",
+        "27. Which is the strongest muscle by force?\n"
+        "a) Heart\nb) Jaw\nc) Thigh\nd) Arm"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -360,10 +415,13 @@ while True:
 
 # Question 28
 while True:
-    a = simpledialog.askstring("Question 28",
-        "28. Largest continent\n"
-        "a) Africa\nb) Asia\nc) Europe\nd) North America")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 28",
+        "28. Which is the largest continent?\n"
+        "a) Africa\nb) Asia\nc) Europe\nd) Australia"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "b":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -372,10 +430,13 @@ while True:
 
 # Question 29
 while True:
-    a = simpledialog.askstring("Question 29",
-        "29. Fastest insect\n"
-        "a) Dragonfly\nb) Horsefly\nc) Hawk Moth\nd) Beetle")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 29",
+        "29. Which insect can fly the fastest?\n"
+        "a) Dragonfly\nb) Bee\nc) Mosquito\nd) Butterfly"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "a":
         messagebox.showinfo("Correct", "Correct!")
         break
@@ -384,12 +445,16 @@ while True:
 
 # Question 30
 while True:
-    a = simpledialog.askstring("Question 30",
-        "30. Most populated country\n"
-        "a) USA\nb) India\nc) China\nd) Russia")
-    if a is None: break
+    a = simpledialog.askstring(
+        "Question 30",
+        "30. Which country has the largest population (traditional GK)?\n"
+        "a) USA\nb) India\nc) China\nd) Russia"
+    )
+    if a is None:
+        break
     elif a.lower().strip() == "c":
         messagebox.showinfo("Correct", "Correct!")
         break
     else:
-        messagebox.showerror("Wrong", "Wrong answer, try again")    
+        messagebox.showerror("Wrong", "Wrong answer, try again")
+messagebox.showinfo("Done", "Test Completed 🎉")
